53200e5b69c324635709701fd1a09d8bd3ef1d18
thanxx <oleksandr.radchenko@microfocus.com>
2023-08-31 19:20:14 +0300

    remove unneeded permissions

--- app/src/main/java/io/tripovan/voltage/utils/Constants.kt
+++ app/src/main/java/io/tripovan/voltage/utils/Constants.kt
@@ -5,4 +5,11 @@ object Constants {
     const val volt2InitialCapacity = 18.4
 
     val largeNumberToExtractFromTs = 1690000000000
+
+    val permissions = arrayOf(
+        "android.permission.BLUETOOTH",
+        "android.permission.BLUETOOTH_ADMIN",
+        "android.permission.BLUETOOTH_CONNECT",
+        "android.permission.BLUETOOTH_SCAN",
+        "android.permission.WRITE_EXTERNAL_STORAGE")
 }
